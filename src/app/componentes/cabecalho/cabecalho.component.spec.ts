728f018e3ffff6ee100baf0d019ec29ad6f0bec5
Gabriel Denti <gabrieldentii@outlook.com.br>
2026-08-25 21:27:14 -0300

    curso angular 14

--- src/app/componentes/cabecalho/cabecalho.component.spec.ts
+++ src/app/componentes/cabecalho/cabecalho.component.spec.ts
@@ -8,10 +8,9 @@ describe('CabecalhoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CabecalhoComponent]
-    })
-    .compileComponents();
-    
+      declarations: [CabecalhoComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CabecalhoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
