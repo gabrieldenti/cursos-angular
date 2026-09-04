de9de7ee438ab28f20f96a992fc5275312d6f858
Gabriel Henrique Marino Denti <gabriel.denti@ogmoes.com.br>
2026-09-04 14:55:14 -0300

    formgroup, builder e validators - continuacao do curso de angular

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,25 +10,26 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit{
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
+  formulario!: FormGroup; //formgroup é uma classe do Angular que representa um grupo de controles de formulário. Ele é usado para agrupar e gerenciar vários controles de formulário relacionados, permitindo que você valide, rastreie o estado e manipule os valores desses controles de forma mais eficiente.
 
-  }
-
-  constructor(private pensamentoService: PensamentoService, private router: Router){
+  constructor(private pensamentoService: PensamentoService, private router: Router, private formBuilder: FormBuilder) { //formbuilder resumindo é uma classe do Angular que fornece uma maneira conveniente de criar instâncias de FormGroup e FormControl. Ele simplifica a criação de formulários reativos, permitindo que você defina a estrutura do formulário e seus controles de forma mais concisa e legível.
 
   }
 
   ngOnInit(): void {
-    
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', [Validators.required, Validators.minLength(3), Validators.pattern(/(.|\s)*\S(.|\s)*/)]],
+      autoria: ['', [Validators.required, Validators.minLength(3), Validators.pattern(/(.|\s)*\S(.|\s)*/)]],
+      modelo: ['', [Validators.required]]
+    })
   }
 
   criarPensamento(){
-    this.pensamentoService.criarPensamento(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento']) //navega para a rota listarPensamento, router.navigate é um método do Router que permite navegar para uma rota específica dentro da aplicação Angular. 
-    }) //subscribe é um método que permite que você se inscreva em um Observable e receba notificações quando novos valores são emitidos. Ele é usado para lidar com operações assíncronas, como chamadas HTTP, eventos de usuário ou qualquer outra fonte de dados que possa emitir valores ao longo do tempo.
+    if(this.formulario.valid){
+      this.pensamentoService.criarPensamento(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento']) //navega para a rota listarPensamento, router.navigate é um método do Router que permite navegar para uma rota específica dentro da aplicação Angular. 
+      }) //subscribe é um método que permite que você se inscreva em um Observable e receba notificações quando novos valores são emitidos. Ele é usado para lidar com operações assíncronas, como chamadas HTTP, eventos de usuário ou qualquer outra fonte de dados que possa emitir valores ao longo do tempo.
+    }
   }
 
   cancelarPensamento(){
